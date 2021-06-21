367a65b1d8cff278324d5c97b42d20f7447e4211
foxstar69 <64551845+foxstar69@users.noreply.github.com>
2021-06-21 20:04:44 +0530

    first

--- weather.py
+++ weather.py
@@ -5,6 +5,7 @@ import time
 
 def getWeather():
     city = textField.get()
+    
     api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
     
     json_data = requests.get(api).json()
@@ -29,8 +30,10 @@ canvas.geometry("600x500")
 canvas.title("Weather App")
 f = ("poppins", 15, "bold")
 t = ("poppins", 35, "bold")
-#heading=tk.Label(canvas,text="enter city name",width = 10, font = t)
-#heading.grid(row=0,column=0,pady=5)
+heading=tk.Label(canvas,text="Enter city name",width = 30, font = 10)
+#heading.grid(canvas,row=1,column=0,bd=1)
+
+heading.pack()
 
 textField = tk.Entry(canvas, justify='left', width = 10, font = t)
 textField.pack(pady = 10)
