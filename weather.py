bf39bcb487e7ae5587fac1ead200885636562a01
foxstar69 <64551845+foxstar69@users.noreply.github.com>
2021-06-21 19:28:29 +0530

    initial

---
+++ weather.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+import requests
+import time
+ 
+
+def getWeather():
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
+    
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    max_temp = int(json_data['main']['temp_max'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
+    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
+
+    final_info = condition + "\n" + str(temp) + "°C" 
+    final_data = "\n"+ "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(max_temp) + "°C" +"\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" +"Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+    label1.config(text = final_info)
+    label2.config(text = final_data)
+ 
+
+canvas = tk.Tk()
+canvas.geometry("600x500")
+canvas.title("Weather App")
+f = ("poppins", 15, "bold")
+t = ("poppins", 35, "bold")
+#heading=tk.Label(canvas,text="enter city name",width = 10, font = t)
+#heading.grid(row=0,column=0,pady=5)
+
+textField = tk.Entry(canvas, justify='left', width = 10, font = t)
+textField.pack(pady = 10)
+textField.focus()
+textField.bind('<Return>', getWeather)
+button = tk.Button(canvas, text='get weather', width=25, command=getWeather)
+button.pack()
+label1 = tk.Label(canvas, font=t)
+label1.pack()
+label2 = tk.Label(canvas, font=f)
+label2.pack()
+canvas.mainloop()
